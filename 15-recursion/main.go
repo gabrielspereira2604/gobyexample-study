ee7eba45c8a0f967788e9e8ee21aa1ab7547d7b3
gabrielspereira2604 <gabrielspereira2604@gmail.com>
2026-03-14 15:53:09 -0300

    15-recursion

---
+++ 15-recursion/main.go
@@ -0,0 +1,28 @@
+package main
+
+import "fmt"
+
+func fact(n int) int {
+	if n == 0 {
+		return 1
+	}
+
+	return n * fact(n-1)
+}
+
+func main() {
+	fmt.Println(fact(7))
+
+	// A função anônima abaixo é atribuída à variável fib, e pode chamar a si mesma para calcular o n-ésimo número de Fibonacci.
+	var fib func(n int) int
+
+	fib = func(n int) int {
+		if n < 2 {
+			return n
+		}
+
+		return fib(n-1) + fib(n-2)
+	}
+
+	fmt.Println(fib(7))
+}
