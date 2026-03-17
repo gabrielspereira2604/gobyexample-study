8ce4264b965f14dd194a948f931e054139a5d210
gabrielspereira2604 <gabrielspereira2604@gmail.com>
2026-03-17 18:13:34 -0300

    21-enums

---
+++ 21-enums/main.go
@@ -0,0 +1,46 @@
+package main
+
+import "fmt"
+
+type ServerState int
+
+const (
+	// iota is a special identifier in Go that is used to create enumerated constants. It starts at 0 and increments by 1 for each subsequent constant in the same block.
+	StateIdle ServerState = iota
+	StateConnected
+	StateError
+	StateRetrying
+)
+
+var stateName = map[ServerState]string{
+	StateIdle:      "idle",
+	StateConnected: "connected",
+	StateError:     "error",
+	StateRetrying:  "retrying",
+}
+
+func (ss ServerState) String() string {
+	return stateName[ss]
+}
+
+func main() {
+	ns := transition(StateIdle)
+	fmt.Println(ns)
+
+	ns2 := transition(ns)
+	fmt.Println(ns2)
+}
+
+func transition(s ServerState) ServerState {
+	switch s {
+	case StateIdle:
+		return StateConnected
+	case StateConnected, StateRetrying:
+
+		return StateIdle
+	case StateError:
+		return StateError
+	default:
+		panic(fmt.Errorf("unknown state: %s", s))
+	}
+}
